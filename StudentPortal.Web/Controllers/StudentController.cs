d1b5220031792546d4e8cc18595ba9bdaed6a75e
ShariarMiraj <shahriarmiraj116@gmail.com>
2024-01-29 13:04:37 +0600

    Student Done

--- StudentPortal.Web/Controllers/StudentController.cs
+++ StudentPortal.Web/Controllers/StudentController.cs
@@ -46,5 +46,66 @@ namespace StudentPortal.Web.Controllers
 
             return View(students);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id )
+        {
+            var student = await dbContext.Students.FindAsync(id);
+
+            return View(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Student  viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if ( student is not null)
+            {
+                student.Name = viewModel.Name;
+                student.Email = viewModel.Email;
+                student.Phone = viewModel.Phone;
+                student.Subscribed = viewModel.Subscribed;
+
+                await dbContext.SaveChangesAsync();
+
+
+            }
+
+            return RedirectToAction("List", "Student");
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.FindAsync(viewModel.Id);
+
+            if (student is not null)
+            {
+                dbContext.Students.Remove(student);
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Student");
+        }
+
+
+        /* [HttpPost]
+        public async Task<IActionResult> Delete(Student viewModel)
+        {
+            var student = await dbContext.Students.
+                AsNoTracking()
+                .FirstOrDefaultAsync( x=> x.Id == viewModel.Id);
+
+            if (student is not null)
+            {
+                dbContext.Students.Remove(viewModel);
+
+                await dbContext.SaveChangesAsync();
+            }
+            return RedirectToAction("List", "Student");
+        }*/
     }
 }
