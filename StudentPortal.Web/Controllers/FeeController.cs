d9ffce1ad9705eab431fc2cf0813fcc3e1660dcc
ShariarMiraj <shahriarmiraj116@gmail.com>
2024-01-31 22:45:00 +0600

    Fee CRUD Opration Done

--- StudentPortal.Web/Controllers/FeeController.cs
+++ StudentPortal.Web/Controllers/FeeController.cs
@@ -46,6 +46,49 @@ namespace StudentPortal.Web.Controllers
            
         }
 
-     
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var fee =await dbContext.fees.FindAsync(id);
+            return View(fee);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Fee viewModel)
+        {
+            var fee = await dbContext.fees.FindAsync(viewModel.FeeId);
+
+            if (fee is not null) 
+            {
+                
+                fee.CourseID = viewModel.CourseID;
+                fee.FeeType = viewModel.FeeType;
+                fee.Amount = viewModel.Amount;
+                fee.LateFee = viewModel.LateFee;
+                fee.PaymentDate = viewModel.PaymentDate;
+
+
+                await dbContext.SaveChangesAsync();
+
+            }
+
+            return RedirectToAction("List", "Fee");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Fee viewModel)
+        {
+            var fee = await dbContext.fees.FindAsync(viewModel.FeeId);
+
+            if (fee is not null)
+            {
+                dbContext.fees.Remove(fee);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction("List", "Fee");
+
+        }
     }
 }
